ce7f7deab5dc415081cb2116873960de4544253a
QwadwoNyamekye <oppongnkrumahdavid@gmail.com>
2022-09-09 22:17:47 -0700

    lll

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -26,20 +26,26 @@ int main(void)
 		}
 		else
 		{
-			putchar(44); putchar(32);
+			putchar(44);
+		       	putchar(32);
 			if ((k == 57) && (l == 57))
 			{
 				if (j < 56)
 				{
-					l = ++j + 1; k = i;
+					l = ++j + 1;
+				       	k = i;
 				}
 				else if (j == 56)
 				{
-					j++; k = i + 1; l = 48;
+					j++;
+				       	k = i + 1; 
+					l = 48;
 				}
 				else if (j == 57)
 				{
-					j = 48; l = 49; k = ++i;
+					j = 48; 
+					l = 49; 
+					k = ++i;
 				}
 			}
 			else if (l < 57)
@@ -48,7 +54,8 @@ int main(void)
 			}
 			else
 			{
-				l = 48; k++;
+				l = 48;
+			       	k++;
 			}
 		}
 	}
