7a493c315f0ed30a37eb2331fe12321695a80223
Qwadwo Nyamekye <38423379+QwadwoNyamekye@users.noreply.github.com>
2022-09-13 22:44:28 +0000

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Description: Prints the alphabet with _putchar
+ *
+ * Return: void
+ */
+
+
+void print_alphabet(void)
+{
+	char char_;
+
+	for (char_ = 'a'; char_ <= 'z'; char_++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
