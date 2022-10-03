c4fa329fb014a83b93354ad7d7d68406e43e4ed6
David Oppong-Nkrumah <oppongnkrumahdavid@gmail.com>
2022-10-03 21:16:06 +0000

    initial commits

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _strdup - returns pointer to a newly allocated
+* space in memory which contains copy of the string passed.
+* @str: pointer to string being duplicated.
+*
+* Return: Pointer to dup string on success
+* NULL if str is NULL.
+* NULL if mem was insufficient.
+*/
+char *_strdup(char *str)
+{
+	char *str2;
+	unsigned int size = 0, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		size++;
+	}
+
+	str2 = malloc(sizeof(char) * (size + 1));
+
+	if (str2 == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		str2[i] = str[i];
+	}
+	return (str2);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* str_concat - concatenats two strings.
+* @s1: pointer to a string.
+* @s2: pointer to string.
+*
+* Return: pointer to newly allocated memory
+* with s1, s2 and null byte.
+* NULL on failure.
+*/
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i = 0, j = 0, ss1 = 0, ss2 = 0;
+	char *res;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[ss1] != '\0')
+		ss1++;
+
+	while (s2[ss2] != '\0')
+		ss2++;
+
+	res = malloc(sizeof(char) * (ss1 + ss2 + 1));
+	if (res == NULL)
+		return (NULL);
+
+	while (i < ss1)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	while (j < ss2)
+	{
+		res[i] = s2[j];
+		i++;
+		j++;
+	}
+	res[i] = '\0';
+	return (res);
+
+}
